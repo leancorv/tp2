d4292015fc9093f0480553e486704d660caceab7
facundolizarraga11 <167789645+facundolizarraga11@users.noreply.github.com>
2025-11-14 14:17:19 -0300

    Remove unused methods from GestorExamenes

--- estudiantes/src/main/java/aed/GestorExamenes.java
+++ estudiantes/src/main/java/aed/GestorExamenes.java
@@ -57,31 +57,4 @@ public class GestorExamenes {
         return cantidadEstudiantes;
     }
     
-    public List<Integer> obtenerRespuestasPorEjercicio(int ejercicio) {
-        List<Integer> respuestas = new ArrayList<>();
-        for (int est = 0; est < cantidadEstudiantes; est++) {
-            respuestas.add(examenes[est][ejercicio]);
-        }
-        return respuestas;
-    }
-    
-    public boolean estaCompleto(int estudiante) {
-        for (int ej = 0; ej < cantidadEjercicios; ej++) {
-            if (examenes[estudiante][ej] == -1) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    public int contarEjerciciosResueltos(int estudiante) {
-        int contador = 0;
-        for (int ej = 0; ej < cantidadEjercicios; ej++) {
-            if (examenes[estudiante][ej] != -1) {
-                contador++;
-            }
-        }
-        return contador;
-    }
-    
 }

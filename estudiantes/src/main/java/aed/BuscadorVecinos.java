df33286038dc488df10ec5c06fcc0e2828663980
facundolizarraga11 <167789645+facundolizarraga11@users.noreply.github.com>
2025-11-14 14:54:29 -0300

    Refactor BuscadorVecinos to improve neighbor logic

--- estudiantes/src/main/java/aed/BuscadorVecinos.java
+++ estudiantes/src/main/java/aed/BuscadorVecinos.java
@@ -1,41 +1,26 @@
 package aed;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class BuscadorVecinos {
+    private int ladoAula;
     private int cantidadEstudiantes;
-    private int cantidadEstudiantesxFila;
     
-    public BuscadorVecinos(int ladoAula, int cantidadEstudiantes){
+    public BuscadorVecinos(int ladoAula, int cantidadEstudiantes) {
+        this.ladoAula = ladoAula;
         this.cantidadEstudiantes = cantidadEstudiantes;
-        this.cantidadEstudiantesxFila = (ladoAula + 1)/2;
-        /*[0] - [1] - [2] -
-          [3] - [4] - [5] -  En una ladoAula = 7, entran 3 por fila, 
-          [6] - [7] - [8] -
-         */
     }
-
-    public List<Integer> obtenerVecinos(int estudianteId){
+    
+    public List<Integer> obtenerVecinos(int estudiante) {
         List<Integer> vecinos = new ArrayList<>();
-
-        // Agrego el vecino de arriba
-        if((estudianteId - cantidadEstudiantesxFila) >= 0){
-            int vecino = estudianteId - cantidadEstudiantes;
-            vecinos.add(vecino);
-        }
-
-        //Agrego el vecino de la izquierda si no esta contra la pared derecha
-        if((estudianteId%cantidadEstudiantes) != 0){
-            vecinos.add(estudianteId-1);
-        }
-
-        //Agrego el vecino de la derecha si no esta contra la pared izquierda
-        int alumnoALaDerecha = cantidadEstudiantes - 1;
-        if(estudianteId%cantidadEstudiantes != alumnoALaDerecha){
-            vecinos.add(estudianteId+1);
-        }
-
+        
+        int fila = estudiante / ladoAula;
+        int columna = estudiante % ladoAula;
+        
+        if (columna > 0) vecinos.add(estudiante - 1);
+        if (columna < ladoAula - 1) vecinos.add(estudiante + 1);
+        if (fila < (cantidadEstudiantes / ladoAula) - 1) vecinos.add(estudiante + ladoAula);
+        
         return vecinos;
     }
 }
